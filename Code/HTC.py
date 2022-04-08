3752e14129956ab7b02cb5467c659f940719faeb
Tommaso <bertola.tommaso@hotmail.it>
2022-04-08 22:48:59 +0200

    Miglioramento di stile su prima simulazione

--- Code/HTC.py
+++ Code/HTC.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numba import jit, prange
 
 class Brain:
     def __init__(self, W) -> None:
@@ -13,61 +14,70 @@ class Brain:
     def set_simulation_parameters(self, dt, n_timesteps, runs):
         self.dt = dt  # timestep
         self.n_timesteps = n_timesteps  # total number of steps in the simulation
-        self.runs = runs  # parallel simulations fof random configs
+        self.n_runs = runs  # parallel simulations fof random configs
 
-    # def simulate(self, threshold_range=None):
-    #    self.neurons_status = np.zeros((self.n_neurons, self.n_timesteps))
-    #    # self.generate_initial_conf()
+ ##################################
 
-    def generate_initial_conf(self, active_fraction=0.3):
-        """
-        Generate initial *random* configurations of states
-        for every neuron in the Brain and for every run
-        """
-        active_neurons = int(
-            active_fraction*self.n_neurons)  # total numeber of active neurons
-        # matrix with random configurations
-        temp_states = np.zeros(self.n_neurons)
 
-        temp_states[0:active_neurons] = 1  # set to 1 active neurons
-        # set to -1 (refractary) the remaining half of neurons
-        temp_states[-(self.n_neurons-active_neurons)//2:] = -1
+@jit(nopython=True)
+def generate_initial_conf(active_frac=0.5, n_neurons=998, n_runs=100):
+    """
+    Generate n_runs initial *random* configurations of states for every n_neuron 
+    """
+    # total numeber of active neurons
+    active_neurons = int(active_frac*n_neurons)
 
-        # create matrix to store initial confs
-        self.states = np.zeros((self.runs,self.n_timesteps, self.n_neurons))
-        # generate {runs} different states
-        # shuffles the original temp_states distribution of active and refracatry neurons
-        for run in range(self.runs):
-            self.states[run,0] = np.random.choice(
-                temp_states, self.n_neurons, replace=False)
-            # (source, output array length, do not take twice)
+    # init of array with random configurations
+    temp_states = np.zeros(n_neurons, dtype=np.float64)
 
-    def compute_parameters(self):
-        pass
+    # set to 1 active neurons
+    # set to -1 (refractary) the remaining half of neurons
+    temp_states[0:active_neurons] = 1
+    temp_states[-(n_neurons-active_neurons)//2:] = -1
 
-    def update_neurons(self, state_neurons):
+    # create matrix to store initial confs
+    states = np.zeros((n_runs, n_neurons), dtype=np.float64)
 
-        # generate n_neurons random numbers
-        p = np.random.random(self.n_neurons)
+    # generate n_runs different states
+    # shuffles the original temp_states
+    # distribution of active and refracatry neurons
+    for run in prange(n_runs):
+        states[run] = np.random.choice(temp_states, n_neurons, replace=False)
+        # (source, output array length, do not take twice)
 
-        # get the array of active neurons
-        active_nodes = (state_neurons == 1).astype(np.float64)
+    return states
 
-        # compute the probability of becoming active for each neuron
-        # and store it inside an array
-        prob_active = self.r1+(1-self.r1)*(self.W@active_nodes > self.tc)
 
-        return ((state_neurons == 1)*(-1) +                # active->inactive
-                (state_neurons == 0)*(p < prob_active) +   # inactive->active following prob_active
-                (state_neurons == -1)*(p > self.r2)*(-1))  # refractary->inactive following r2
+@jit(nopython=True)
+def update_neurons(state_neurons, r1, r2, tc, W):
+    # generate n_neurons random numbers
+    p = np.random.random(state_neurons.shape[0])
 
-    def update_status(self):
-        print("Randomization")
-        for run in range(self.runs):
-            self.states[run,0] = self.update_neurons(self.states[run,0])
-        print("End randomization")
+    # get the array of active neurons
+    active_nodes = (state_neurons == 1).astype(np.float64)
 
-        for run in range(self.runs):
-            for time in range(1,self.n_timesteps):
-                self.states[run,time]=self.update_neurons(self.states[run,time-1])
-            print("End run "+str(run))
+    # compute the probability of becoming active for each neuron
+    # and store it inside an array
+    prob_active = r1+(1-r1)*(W@active_nodes > tc)
+
+    # active->inactive
+    # inactive->active following prob_active
+    # refractary->inactive following r2
+    return ((state_neurons == 1)*(-1) +
+            (state_neurons == 0)*(p < prob_active) +
+            (state_neurons == -1)*(p > r2)*(-1))
+
+
+@jit(nopython=True)#, parallel=True)
+def update_states(r1, r2, tc, W, states):
+    # save the number of runs in total
+    n_runs = states.shape[0]
+
+    # temp states with same dims as states
+    temp_states = np.zeros(states.shape, dtype=np.float64)
+
+    # compute for each run the new activity status
+    for run in prange(n_runs):
+        temp_states[run] = update_neurons(states[run], r1, r2, tc, W)
+
+    return temp_states, (temp_states==1).astype(np.float64)
