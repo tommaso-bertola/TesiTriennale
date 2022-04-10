158fd4ad282b751815e481674fc73fade4afaaae
Tommaso <bertola.tommaso@hotmail.it>
2022-04-10 22:47:09 +0200

    Simulazione con range tc e calcolo A

--- Code/HTC.py
+++ Code/HTC.py
@@ -1,20 +1,74 @@
 import numpy as np
 from numba import jit, prange
 
+
 class Brain:
     def __init__(self, W) -> None:
         self.W = W
         self.n_neurons = W.shape[0]
 
-    def set_netowrk_parameters(self, r1, r2, tc):
+    def set_netowrk_parameters(self, r1, r2, tc, tmin=0.01, tmax=3, delta_tc=0.1):
         self.r1 = r1
         self.r2 = r2
         self.tc = tc
+        self.tmin = tmin
+        self.tmax = tmax
+        self.delta_tc = delta_tc
 
     def set_simulation_parameters(self, dt, n_timesteps, runs):
         self.dt = dt  # timestep
         self.n_timesteps = n_timesteps  # total number of steps in the simulation
         self.n_runs = runs  # parallel simulations fof random configs
+
+    def simulation(self, active_frac=0.1, n_runs=100):
+        n_neurons = self.W.shape[0]
+
+        # Array of the tested tc for each simulation
+        tc = np.arange(self.tmin, self.tmax, self.delta_tc, dtype=np.float64)
+
+        # Array containing tc and associated activity
+        activity = np.zeros_like(tc, dtype=np.float64)
+
+        # Matrix containing activities of neurons for each run
+        activity_t = np.zeros((self.n_timesteps), dtype=np.float64)
+
+        # Init of random states for the simulation
+        states_init = generate_initial_conf(active_frac, n_neurons, n_runs)
+
+        # For every tc
+        for i, t in enumerate(tc):
+
+            # Copy the random states
+            states = states_init
+
+            # Init a temporary obj to store all the activity (for each run and neuron)
+            temp_act = np.zeros(states.shape, dtype=np.float64)
+
+            # Initial adjustment of the system
+            for dummy in range(100):
+                states, temp_act = update_states(
+                    self.r1, self.r2, t, self.W, states)
+
+            # Real simulation
+            # For each time step:
+            for time in range(self.n_timesteps):
+
+                # Save states and activity
+                states, temp_act = update_states(
+                    self.r1, self.r2, t, self.W, states)
+
+                # Compute activty at time t
+                # Sum all 1s in the temp_act matrix
+                # and divide by total number of neurons and runs
+                activity_t[time] = ((temp_act.sum()).sum())/(n_neurons*n_runs)
+
+            # Save the mean activity for the simulation at chosen tc
+            activity[i] = activity_t.sum()/(self.dt*self.n_timesteps)
+
+            print("End of ", t)
+
+        # Return vector of tc and associated activities
+        return (tc, activity)
 
  ##################################
 
@@ -68,7 +122,7 @@ def update_neurons(state_neurons, r1, r2, tc, W):
             (state_neurons == -1)*(p > r2)*(-1))
 
 
-@jit(nopython=True)#, parallel=True)
+@jit(nopython=True)  # , parallel=True)
 def update_states(r1, r2, tc, W, states):
     # save the number of runs in total
     n_runs = states.shape[0]
@@ -80,4 +134,4 @@ def update_states(r1, r2, tc, W, states):
     for run in prange(n_runs):
         temp_states[run] = update_neurons(states[run], r1, r2, tc, W)
 
-    return temp_states, (temp_states==1).astype(np.float64)
+    return temp_states, (temp_states == 1).astype(np.float64)
